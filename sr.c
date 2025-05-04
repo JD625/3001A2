00dd67ef0e1d18808303ea063f59e4e287d9b3d9
Jerry <a1888643@adelaide.edu.au>
2025-05-05 00:11:59 +0930

    Noticed timer was being restarted for every packet resend — moved it to start once after loop

--- sr.c
+++ sr.c
@@ -68,6 +68,8 @@ void A_input(struct pkt packet)
   int i;
 
   if (!IsCorrupted(packet)) {
+    if (TRACE > 0)
+      printf("----A: uncorrupted ACK %d is received\n",packet.acknum);
     total_ACKs_received++;
 
     if (windowcount != 0) {
@@ -78,11 +80,11 @@ void A_input(struct pkt packet)
 
         new_ACKs++;
 
-      if (packet.acknum >= seqfirst)
-        ackcount = packet.acknum - seqfirst; 
-      else
-        ackcount = SEQSPACE - seqfirst + packet.acknum;
-      
+        if (packet.acknum >= seqfirst)
+          ackcount = packet.acknum + 1 - seqfirst;
+        else
+          ackcount = SEQSPACE - seqfirst + packet.acknum;
+
         windowfirst = (windowfirst + ackcount) % WINDOWSIZE;
 
         for (i=0; i<ackcount; i++)
@@ -93,8 +95,10 @@ void A_input(struct pkt packet)
           starttimer(A, RTT);
       }
     }
-  }
+  } else if (TRACE > 0)
+    printf ("----A: corrupted ACK is received, do nothing!\n");
 }
+
 
 void A_timerinterrupt(void)
 {
@@ -103,14 +107,17 @@ void A_timerinterrupt(void)
   if (TRACE > 0)
     printf("----A: time out,resend packets!\n");
 
-  for(i=0; i<windowcount; i++) {
+  for(i = 0; i < windowcount; i++) {
     if (TRACE > 0)
       printf ("---A: resending packet %d\n", (buffer[(windowfirst+i) % WINDOWSIZE]).seqnum);
 
-    tolayer3(A,buffer[(windowfirst+i) % WINDOWSIZE]);
+    tolayer3(A, buffer[(windowfirst+i) % WINDOWSIZE]);
     packets_resent++;
- }
+  }
+  if (windowcount > 0)
+    starttimer(A, RTT);
 }
+
 void A_init(void)
 {
   A_nextseqnum = 0;
