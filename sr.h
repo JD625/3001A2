79c2769222125408c5f07357a962c9ef750ba3f4
Jerry <a1888643@adelaide.edu.au>
2025-05-05 15:53:25 +0930

    Finalise

--- sr.h
+++ sr.h
@@ -5,7 +5,7 @@ extern void B_input(struct pkt);
 extern void A_output(struct msg);
 extern void A_timerinterrupt(void);
 
-/* included for extension to bidirectional communication */
-#define BIDIRECTIONAL 0       /*  0 = A->B  1 =  A<->B */
+
+#define BIDIRECTIONAL 0      
 extern void B_output(struct msg);
 extern void B_timerinterrupt(void);
